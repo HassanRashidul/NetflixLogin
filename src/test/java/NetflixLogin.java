6ad6937ff46be0c0147acbe171ff2e954fc7b16e
HassanRashidul <rashidul.h.hassan@gmail.com>
2021-10-20 04:13:15 +0600

    20/10/2021

--- src/test/java/NetflixLogin.java
+++ src/test/java/NetflixLogin.java
@@ -18,17 +18,94 @@ public class NetflixLogin {
     static JavascriptExecutor js;
 
     @BeforeTest
-    public static void WebSetup() throws InterruptedException {
+    public static void WebSetup() throws Exception {
+        MyScreenRecorder.startRecording("");
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(BaseURL);
         Thread.sleep(3000);
+        MyScreenRecorder.stopRecording();
+    }
+
+    @Test(priority = 0)
+    public static void TestCase01() throws Exception {
+        MyScreenRecorder.startRecording("");
+        driver.findElement(By.xpath("//*[@class='authLinks redButton']")).click();
+        Thread.sleep(3000);
+        MyScreenRecorder.stopRecording();
     }
 
     @Test(priority = 1)
-    public static void UserLogin() throws InterruptedException {
-        driver.findElement(By.xpath("//*[@class='authLinks redButton']")).click();
+    public static void TestCase02() throws Exception {
+        MyScreenRecorder.startRecording("");
+        driver.findElement(By.xpath("//input[@id='id_userLoginId']")).sendKeys("jueal12@gmail.com");
+        driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("ABCabc@12");
+        driver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).click();
         Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@id='id_userLoginId']")).clear();
+        driver.findElement(By.xpath("//input[@id='id_password']")).clear();
+        MyScreenRecorder.stopRecording();
+    }
+
+    @Test(priority = 2)
+    public static void TestCase03() throws Exception {
+        MyScreenRecorder.startRecording("");
+        driver.findElement(By.xpath("//input[@id='id_userLoginId']")).sendKeys("rashidul.h.hassan@gmail.com");
+        driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("ABCabc@12");
+        driver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Sign In')]")).click();
+        MyScreenRecorder.stopRecording();
+    }
+
+    @Test(priority = 3)
+    public static void TestCase04() throws Exception {
+        MyScreenRecorder.startRecording("");
+
+        driver.findElement(By.xpath("//input[@id='id_userLoginId']")).sendKeys("01701040102");
+        driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("ABCabc@12");
+        driver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@id='id_userLoginId']")).clear();
+        driver.findElement(By.xpath("//input[@id='id_password']")).clear();
+        MyScreenRecorder.stopRecording();
+    }
+
+    @Test(priority = 4)
+    public static void TestCase05() throws Exception {
+        MyScreenRecorder.startRecording("");
+        driver.findElement(By.xpath("//input[@id='id_userLoginId']")).sendKeys("01404047804");
+        driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("ABCabc@12");
+        driver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Sign In')]")).click();
+        MyScreenRecorder.stopRecording();
+    }
+
+    @Test(priority = 5)
+    public static void TestCase06() throws Exception {
+        MyScreenRecorder.startRecording("");
+        driver.findElement(By.xpath("//input[@id='id_userLoginId']")).sendKeys("01404047804");
+        driver.findElement(By.xpath("//input[@id='id_password']")).sendKeys("ABCabc");
+        driver.findElement(By.xpath("//button[contains(text(),'Sign In')]")).click();
+        Thread.sleep(3000);
+        MyScreenRecorder.stopRecording();
+
+    }
+
+    @Test(priority = 6)
+    public static void TestCase07() throws Exception {
+        MyScreenRecorder.startRecording("");
+        driver.get("https://www.netflix.com/bd/login");
+        driver.findElement(By.xpath("//span[contains(text(),'Login with Facebook')]")).click();
+        MyScreenRecorder.stopRecording();
+    }
+    @AfterTest
+    public static void TestClosure(){
+
+        driver.quit();
     }
 }
